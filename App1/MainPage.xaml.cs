b177265d167b899e8f401c2e216cb41ecbbcbeb7
Hugo Santiago Castro <104536146+hugosantiagocastro90@users.noreply.github.com>
2022-05-04 07:21:33 -0500

    Agregar archivos de proyecto.

---
+++ App1/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            double notaSeguimiento = 0,
+            examen = 0,
+            parcial = 0,
+            notaSeguimiento2 = 0,
+            examen2 = 0,
+            parcial2 = 0,
+            notaFinal = 0;
+            string estado = "";
+            try
+            {
+                notaSeguimiento = Convert.ToDouble(txtSeguimiento.Text);
+                examen = Convert.ToDouble(txtExamen.Text);
+                parcial = (notaSeguimiento * 0.3) + (examen * 0.2);
+                lblNotaParcial.Text = parcial.ToString();
+                notaSeguimiento2 = Convert.ToDouble(txtSeguimiento2.Text);
+                examen2 = Convert.ToDouble(txtExamen2.Text);
+                parcial2 = (notaSeguimiento2 * 0.3) + (examen2 * 0.2);
+                lblNotaParcial2.Text = parcial2.ToString();
+                notaFinal = parcial + parcial2;
+                lblNota.Text = notaFinal.ToString();
+                if (notaFinal >= 7)
+                {
+                    estado = "Aprobado";
+                }
+                else if (notaFinal >= 5 && notaFinal <= 6.9)
+                {
+                    estado = "Complementario";
+                }
+                else
+                {
+                    estado = "Reprobado";
+                }
+                DisplayAlert("Estado Final", "Estado es :" + estado, "Cancelar"); ;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void btnLimpiar_Clicked(object sender, EventArgs e)
+
+        {
+        }
+    }
+}
---
+++ App1/Page1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "";
+            string validar = validarInicio(txtusuario.Text, txtContrasena.Text);
+            if (validar == "Error")
+            {
+                DisplayAlert("Usuario o Contraseña", validar, "Continuar");
+            }
+            else
+            {
+                usuario = txtusuario.Text;
+                Navigation.PushAsync(new MainPage());
+            }
+        }
+            private string validarInicio(string usuario, string pass)
+            {
+                string validacion = "";
+                if (usuario == "santiago2022" && pass == "uisrael2022")
+                {
+                    validacion = "";
+
+                }
+                else
+                {
+                    validacion = "Error";
+                }
+                return validacion;
+            }
+      
+    }
+}
